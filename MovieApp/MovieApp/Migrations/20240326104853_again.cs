4b329f4809bede974157ffbca2b9eaf49225a31b
ElenaaAtanasoska <107962440+ElenaaAtanasoska@users.noreply.github.com>
2024-03-26 17:12:20 +0100

    a

---
+++ MovieApp/MovieApp/Migrations/20240326104853_again.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class again : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OwnerId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_AspNetUsers_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TicketOrders",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TicketId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OrderId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TicketOrders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TicketOrders_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TicketOrders_Tickets_TicketId",
+                        column: x => x.TicketId,
+                        principalTable: "Tickets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_OwnerId",
+                table: "Orders",
+                column: "OwnerId",
+                unique: true,
+                filter: "[OwnerId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TicketOrders_OrderId",
+                table: "TicketOrders",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TicketOrders_TicketId",
+                table: "TicketOrders",
+                column: "TicketId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TicketOrders");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
---
+++ MovieApp/MovieApp/Models/DTO/AddToCartDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieApp.Models.DTO
+{
+    public class AddToCartDTO
+    {
+        public Guid SelectedTicketId { get; set; }
+        public double? SelectedTicketPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
